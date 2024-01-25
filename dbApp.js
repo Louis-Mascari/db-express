0ac7fd50bfaf1a6580227997e59c9bea0798e60c
Louis-Mascari <Lmascari16@gmail.com>
2024-01-24 20:43:45 -0500

    testing

--- dbApp.js
+++ dbApp.js
@@ -11,20 +11,21 @@ const pool = new Pool({
   port: 5432,
 });
 
-app.get('/friends', async (req, res) => {
-    console.log('made it here');
-    try {
-      console.log('Request received for /friends'); // Add this line
-      const result = await pool.query('SELECT * FROM friends');
-      res.json(result.rows);
-    } catch (error) {
-      console.error('Error querying database:', error);
-      res.status(500).send('Internal Server Error');
-    }
+app.get('/friends', (req, res) => {
+    // console.log('made it here');
+    // try {
+    //   console.log('Request received for /friends'); // Add this line
+    //   const result = await pool.query('SELECT * FROM friends');
+    //   res.json(result.rows);
+    // } catch (error) {
+    //   console.error('Error querying database:', error);
+    //   res.status(500).send('Internal Server Error');
+    // }
+    res.send('This is my friends list!')
   });
   
 
 // Start the Express server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on ${port}`);
 });
