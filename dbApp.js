8c6891a38a84bae52d84a78a33f5e22909295a7e
Louis-Mascari <Lmascari16@gmail.com>
2024-01-24 19:41:43 -0500

    adding error logging

--- dbApp.js
+++ dbApp.js
@@ -13,14 +13,14 @@ const pool = new Pool({
 
 // Express route to get a list of friends
 app.get('/friends', async (req, res) => {
-  try {
-    // Query the database
-    const result = await pool.query('SELECT * FROM friends');
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Error querying database:', error);
-    res.status(500).send('Internal Server Error');
-  }
+    try {
+      // Query the database
+      const result = await pool.query('SELECT * FROM friends');
+      res.json(result.rows);
+    } catch (error) {
+      console.error('Error querying database:', error); // Log the error
+      res.status(500).send('Internal Server Error');
+    }
 });
 
 // Start the Express server
